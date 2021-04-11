fbbfff092dcd63f1995d193f34c0f6cbcc234d24
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2021-04-11 00:03:59 -0400

    Fire denor.ts after build finishes

--- core/dev/denor.ts
+++ core/dev/denor.ts
@@ -1,13 +1,33 @@
 import * as watcher from "./src/watcher.ts";
 import { exec } from "https://deno.land/x/exec/mod.ts";
 //live reloading watching all files components for changes and will automatically run exec() code once there are changes
+
+// emitter.on("SayHello", (to: string) => {
+//   if (!to) {
+//     console.log("hello!");
+//   } else {
+//     console.log("hello" + to + "!");
+//   }
+// });
+
+// emitter.emit("SayHello");
+// // hello!
+// emitter.emit("SayHello", " world");
+// // hello world!
+// emitter.emit("SayHello", ", again, world");
+// hello, again, world!
+
 async function main() {
   console.log("Watching for file changes.");
   await watcher.watchChanges(".", async () => {
     console.log("File change detected.");
+    console.log("updated denor");
     // Update the vno link to deno 3rd party link when all work is complete
     await exec(
       "deno run --allow-read --allow-write --allow-net --unstable https://raw.githubusercontent.com/oslabs-beta/vno/reloading/install/vno.ts build --ssr",
+    );
+    await exec(
+      `deno run --allow-read --allow-run --allow-write --allow-net --unstable https://raw.githubusercontent.com/oslabs-beta/vno/reloading/core/dev/denor.ts`,
     );
   });
 }
