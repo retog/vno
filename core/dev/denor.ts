d30a7f67d292e3deb3b9fc7cf2dd572572c7c7f8
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2021-04-11 00:35:58 -0400

    Reinvoke denor.ts file after build completes
      Ran into many issues around denon restarting the server and
      none of the updated Vue templates would even start the build
      process.
    
      Suspect that since Vue templates were updated and denon
      detected that, it immediately restarted the denor.ts before any
      other files could be generated by the build.
    
      Stopped using denon to simply run denor.ts with deno run, and
      forced denor.ts to run itself with exec command.
    
      This seems to work OK. Will stick with that, and no need for an
      extra library in here.

--- core/dev/denor.ts
+++ core/dev/denor.ts
@@ -2,24 +2,11 @@
 import { exec } from "https://deno.land/x/exec/mod.ts";
 //live reloading watching all files components for changes and will automatically run exec() code once there are changes
 
-// emitter.on("SayHello", (to: string) => {
-//   if (!to) {
-//     console.log("hello!");
-//   } else {
-//     console.log("hello" + to + "!");
-//   }
-// });
-
-// emitter.emit("SayHello");
-// // hello!
-// emitter.emit("SayHello", " world");
-// // hello world!
-// emitter.emit("SayHello", ", again, world");
-// hello, again, world!
 async function watchChanges(
   path: string,
   onChange: Function,
 ) {
+  console.log("invoking watchChanges");
   const watcher = Deno.watchFs(path);
   //watches a path which (currently not given specific path)
   for await (const event of watcher) {
@@ -32,17 +19,19 @@ async function watchChanges(
   }
 }
 
+//denon/deno command will stop working after a couple rebuilds because needs dtime to finish bundling and compiling process
 async function main() {
   console.log("Watching for file changes.");
-  await watchChanges(".", async () => {
+
+  return await watchChanges(".", async () => {
     console.log("File change detected.");
     console.log("updated denor");
-    // Update the vno link to deno 3rd party link when all work is complete
+    // Update the vno link to deno 3rd party link when all work is complete and uploaded.
     await exec(
-      "deno run --allow-read --allow-write --allow-net --unstable https://raw.githubusercontent.com/oslabs-beta/vno/reloading/install/vno.ts build --ssr",
+      "deno run --allow-read --allow-write --allow-net --unstable ./install/vno.ts build --ssr",
     );
     await exec(
-      `deno run --allow-read --allow-run --allow-write --allow-net --unstable https://raw.githubusercontent.com/oslabs-beta/vno/reloading/core/dev/denor.ts`,
+      `deno run --allow-read --allow-run --allow-write --allow-net --unstable ./core/dev/denor.ts`,
     );
   });
 }
