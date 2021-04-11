68a43c93d7bc85526b8427de3d16aba03052c5f9
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2021-04-11 00:50:52 -0400

    Refactor watcher logic
      Removed watcher.ts file, as it wasn't being used anywhere else
      besides denor.ts.
    
      Made the watchAndRebuild function more extnesible to both
      support SSR and nonSSR builds.
    
      Typed the options object.

--- core/dev/denor.ts
+++ core/dev/denor.ts
@@ -1,16 +1,12 @@
-// import * as watcher from "./src/watcher.ts";
 import { exec } from "https://deno.land/x/exec/mod.ts";
-//live reloading watching all files components for changes and will automatically run exec() code once there are changes
 
 async function watchChanges(
   path: string,
-  onChange: Function,
+  onChange: () => void, //typescript for callback that doesnt return anything
 ) {
-  console.log("invoking watchChanges");
   const watcher = Deno.watchFs(path);
   //watches a path which (currently not given specific path)
   for await (const event of watcher) {
-    console.log(event);
     if (event.kind === "modify") {
       await onChange();
       return;
@@ -19,20 +15,24 @@ async function watchChanges(
   }
 }
 
-//denon/deno command will stop working after a couple rebuilds because needs dtime to finish bundling and compiling process
-async function main() {
-  console.log("Watching for file changes.");
+//live reloading watching all files components for changes and will automatically run exec() code once there are changes
+interface watchOptions {
+  ssr?: boolean;
+}
 
-  return await watchChanges(".", async () => {
+async function watchAndRebuild(options: watchOptions) {
+  const ssrFlag = options?.ssr ? " --ssr" : "";
+  console.log("Watching for changes.");
+  await watchChanges(".", async () => {
     console.log("File change detected.");
-    console.log("updated denor");
     // Update the vno link to deno 3rd party link when all work is complete and uploaded.
     await exec(
-      "deno run --allow-read --allow-write --allow-net --unstable ./install/vno.ts build --ssr",
+      `deno run --allow-read --allow-write --allow-net --unstable ./install/vno.ts build${ssrFlag}`,
     );
     await exec(
       `deno run --allow-read --allow-run --allow-write --allow-net --unstable ./core/dev/denor.ts`,
     );
   });
 }
-main();
+
+watchAndRebuild({ ssr: true });
