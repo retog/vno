071721c01bb0010269ab426e4223eb46e34f6ca9
ubaig517 <ubaig517>
2021-07-06 17:16:28 -0400

    remove ssg files from core

--- core/ssg/reload.js
+++
@@ -1,7 +0,0 @@
-const socket = new WebSocket('ws://localhost:8080');
-socket.addEventListener('message', (event) => {
-  console.log(event);
-  if (event.data === 'reload') {
-    window.location.reload();
-  }
-});
--- core/ssg/vno.ts
+++
@@ -1,7 +0,0 @@
-import { startDev } from './dev-server.ts';
-
-const res = prompt('start the server?:');
-
-if (res?.startsWith('y')) {
-  startDev();
-}
